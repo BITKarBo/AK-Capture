d97a5121561e1a6e35019efeba4a3b019e721da7
KarBo <Karbob125@gmail.com>
2021-10-15 18:08:09 +0300

    Pohjatiedostoja

--- src/core/main.java
+++ src/core/main.java
@@ -1,11 +1,60 @@
 package core;
 
+import java.awt.AWTException;
+import java.awt.Rectangle;
+import java.awt.Robot;
+import java.awt.Toolkit;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
 public class main {
+	static boolean capturing=false; //kuvaus boolean
+	
+	static int x1=0; // valittu alue coordit?? placeholder voidaa miettii myöhemmi
+	static int x2=0;
+	static int y1=0;
+	static int y2=0;
+	
+	//test
+
+	
+	public void capture() {
+		if(capturing=false) {
+		capturing=true;
+		
+		while (capturing) {
+			//lisätään kuvii kansioon
+		}
+		}
+	}
+	public void stopCapture() {
+		capturing=false;
+		//tähän sit vaikka se gif luonti tai uus metodi
+	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		//TESTI COMMENTTI
-		//testi 2
+		//tänne sit vaikka ne key listenerit jotka toteuttaa metodit riippuen keybindista ?
+		 try {
+	            Robot robot = new Robot();
+	 
+	            Rectangle rectangle = new Rectangle(Toolkit.getDefaultToolkit().getScreenSize());
+	           
+	            BufferedImage bufferedImage = robot.createScreenCapture(rectangle);
+	            File file = new File("screen-capture.png");
+	            boolean status = ImageIO.write(bufferedImage, "png", file);
+	            System.out.println("Screen Captured ? " + status + " File Path:- " + file.getAbsolutePath());
+	 
+	        } catch (AWTException | IOException ex) {
+	            System.err.println(ex);
+	        }
+		 
+		 
+		 
+		 
+
 	}
 
 }
